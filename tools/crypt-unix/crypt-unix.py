544933ddab224d488e50ff4ba7b3cca32bf9eaec
L-seaung <2393943608@qq.com>
2017-11-12 17:42:07 +0800

    sun Nov 12, 17:42:05

--- tools/crypt-unix/crypt-unix.py
+++ tools/crypt-unix/crypt-unix.py
@@ -5,9 +5,10 @@ import optparse
 import threading
 
 
-def cryptPass(cryptPassword):
+
+def cryptPass(cryptPassword, dictionary):
     salt = cryptPassword[0:2]
-    dictFile = open('dictionary.txt', 'r')
+    dictFile = open(dictionary, 'r')
 
     for word in dictFile.readlines():
         word = word.strip('\n')
@@ -18,14 +19,44 @@ def cryptPass(cryptPassword):
         print('[-] not found password \n ' )
         return
 
+
 def main():
-    passwordfile = open('password.txt', 'r')
-    for line in passwordfile.readlines():
-        if ':' in line:
-            user = line.split(':')[0]
-            password = line.split(':')[1].strip(' ')
-            print('[*] Cracking password for : user')
-            cryptPass(password)
+    parser = optparse.OptionParser(
+        'usage %prog -f <target password file name> -d <target dictionary file name>'
+    )
+
+    parser.add_option('-f', dest='fname', type='string', help='specify target password file name.')
+    parser.add_option('-d', dest='dname', type='string', help='specify target dictionary file name.')
+
+    (options, args) = parser.parse_args()
+
+    if (options.fname == None | options.dname == None):
+        print(parser.usage)
+        exit(0)
+    else:
+        fname = options.fname
+        dname = options.dname
+        passwordFile = open(fname, 'r')
+        for line in passwordFile.readlines():
+            if ':' in line:
+                user = line.split(':')[0]
+                password = line.split(':')[1].strip(' ')
+                print('[*] Cracking password for : user')
+                t = threading.Thread(target=cryptPass, args=(password, dname))
+                t.start()
 
 if __name__ == '__main__':
     main()
+
+
+# def main():
+#     passwordfile = open('password.txt', 'r')
+#     for line in passwordfile.readlines():
+#         if ':' in line:
+#             user = line.split(':')[0]
+#             password = line.split(':')[1].strip(' ')
+#             print('[*] Cracking password for : user')
+#             cryptPass(password)
+
+# if __name__ == '__main__':
+#     main()
